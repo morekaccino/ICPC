2d12bcbfa29d1d02296be90116347f1de4d25f5a
mowhamadrexa <mrkazemi95@hotmail.com>
2018-12-23 00:34:40 -0500

    study

---
+++ cpp_basics.cpp
@@ -0,0 +1,26 @@
+//
+// Created by mrkaz on 2018-12-22.
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    //solution comes here
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a, b;
+    cin >> a >> b;
+    cout << "a is " << a << endl;
+    cout << "b is " << b << endl;
+
+    scanf("%d %d", &a, &b);
+    printf("a is %d\n", a);
+    printf("b is %d\n", b);
+
+    string s;
+    getline(cin, s);
+    cout << "s is: " << s;
+}
